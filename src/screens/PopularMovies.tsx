0893b353f093485b8fc6c63d2f4f699737f63c84
Chandima <chandima@rhino-partners.com>
2022-01-24 22:18:02 +0530

    Add Fonts

--- src/screens/PopularMovies.tsx
+++ src/screens/PopularMovies.tsx
@@ -1,11 +1,20 @@
 import * as React from 'react';
 
 import {Button, Text, View} from 'react-native';
+import fonts from 'res/fonts';
 
 export default ({navigation}) => {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
+      <Text style={{fontFamily: fonts.regular, fontSize: 20, color: 'black'}}>
+        Home Screen
+      </Text>
+      <Text style={{fontFamily: fonts.light, fontSize: 20, color: 'black'}}>
+        Home Screen
+      </Text>
+      <Text style={{fontFamily: fonts.bold, fontSize: 20, color: 'black'}}>
+        Home Screen
+      </Text>
       <Button
         title="Go to Details"
         onPress={() => navigation.navigate('Details')}
